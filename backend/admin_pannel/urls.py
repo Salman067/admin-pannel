be09e879c643b3f9c4a6872b26984733c97d2362
Salman067 <salman120522@gmail.com>
2024-12-09 10:16:59 +0600

    fixed error

--- backend/admin_pannel/urls.py
+++ backend/admin_pannel/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/',views.Register.as_view(),name='register'),
-    path('login/',views.LoginView.as_view(),name='login'),
-    path('user/list',views.UserList.as_view()), 
-     path('user/view',views.UserView.as_view()),  
-    path('logout/',views.LogOutView.as_view()),  
+    path('/register',views.registration,name='register'),
+    path('/login',views.login,name='login'),
+    path('/list',views.user_details,name='user_details'), 
+    path('/view/<int:id>',views.user_details,name='user_details'),
+    path('/change-password', views.change_password, name='change_password'),  
+    path('/logout',views.logout,name='logout'),  
 ]
--- backend/admin_pannel/views.py
+++ backend/admin_pannel/views.py
@@ -3,85 +3,103 @@ from . import serializers
 from . import models
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated,AllowAny
+import datetime
+from rest_framework_simplejwt.tokens import RefreshToken,TokenError
+from django.contrib.auth import update_session_auth_hash
 
-import jwt, datetime
-
-class Register(APIView):
-    def post(self, request):
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def registration(request):
         serializer = serializers.UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({'status': 'success', 'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
         else:
             return Response({'status': 'error', 'message': 'Invalid data', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    
 
-class LoginView(APIView):
-    def post(self, request):
-        email = request.data.get('email')
-        password = request.data.get('password')
-
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def login(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+    try:
         user = models.User.objects.filter(email=email).first()
-        if user is None:
-            return Response({'status': 'error', 'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
-        if not user.check_password(password):
-            return Response({'status': 'error', 'message': 'Incorrect password'}, status=status.HTTP_401_UNAUTHORIZED)
+        if user and user.check_password(password):
+            refresh = RefreshToken.for_user(user)
+            refresh['user_type'] = user.user_type  
+            refresh['id'] = user.id
+            refresh['email'] = user.email
+            refresh['exp'] = int((datetime.datetime.utcnow() + datetime.timedelta(minutes=30)).timestamp())
+            refresh['iat'] = int(datetime.datetime.utcnow().timestamp())
+            return Response({
+                'refresh_token': str(refresh),
+                'access_token': str(refresh.access_token),
+                'message': 'Logged in successfully'
+            })
+        else:
+            return Response({'status': 'error', 'message': 'Incorrect email or password'}, status=status.HTTP_401_UNAUTHORIZED)
+    except models.User.DoesNotExist:
+        return Response({'status': 'error', 'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
-        payload = {
-            'id': user.id,
-            'email': user.email,
-            'user_type': user.user_type,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
-            'iat': datetime.datetime.utcnow()
-        }
-        token = jwt.encode(payload, 'secret_key', algorithm='HS256')
 
-        response = Response()
-        response.set_cookie(key='token', value=token, httponly=True)
-        response.data = {
-            'message': 'Login successful',
-            'token': token,
-        }
-        return response
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def change_password(request):
+    user = request.user
+    if not user.is_authenticated:
+        return Response({"error": "Authentication required."}, status=status.HTTP_401_UNAUTHORIZED)
+    old_password = request.data.get("old_password")
+    new_password = request.data.get("new_password")
 
-class UserList(APIView):
-    def get(self, request):
-        token = request.COOKIES.get('token')
-        if not token:
-            return Response({'status': 'error', 'message': 'Unauthenticated!'}, status=status.HTTP_401_UNAUTHORIZED)
+    if not old_password or not new_password:
+        return Response({"error": "Both old and new passwords are required."}, status=status.HTTP_400_BAD_REQUEST)
+    
+    if not user.check_password(old_password):
+        return Response({"error": "Old password is incorrect"}, status=status.HTTP_400_BAD_REQUEST)
 
+    if old_password == new_password:
+        return Response({"error": "New password cannot be the same as the old password"}, status=status.HTTP_400_BAD_REQUEST)
+
+    user.set_password(new_password)
+    user.save()
+
+    update_session_auth_hash(request, user)
+
+    return Response({"message": "Password changed successfully"}, status=status.HTTP_200_OK)
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def user_details(request,id=None):
+    if id:
         try:
-            payload = jwt.decode(token, 'secret_key', algorithms=['HS256'])
-        except jwt.ExpiredSignatureError:
-            return Response({'status': 'error', 'message': 'Unauthenticated!'}, status=status.HTTP_401_UNAUTHORIZED)
-
-        user=models.User.objects.all()
-        if not user:
+            user=models.User.objects.get(id=id)
+            serializer=serializers.UserSerializer(user)
+            return Response(serializer.data)
+        except models.User.DoesNotExist:
             return Response({'status': 'error', 'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = serializers.UserSerializer(user,many=True)
-        return Response(serializer.data)
-
-class UserView(APIView):
-    def get(self, request):
-        token = request.COOKIES.get('token')
-        if not token:
-            return Response({'status': 'error', 'message': 'Unauthenticated!'}, status=status.HTTP_401_UNAUTHORIZED)
-
+        
+    else:
         try:
-            payload = jwt.decode(token, 'secret_key', algorithms=['HS256'])
-        except jwt.ExpiredSignatureError:
-            return Response({'status': 'error', 'message': 'Unauthenticated!'}, status=status.HTTP_401_UNAUTHORIZED)
+            user=models.User.objects.all()
+            serializer=serializers.UserSerializer(user,many=True)
+            return Response(serializer.data)
+        except models.User.DoesNotExist:
+            return Response({'status': 'error', 'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)    
 
-        user = models.User.objects.filter(id=payload['id']).first()
-        if not user:
-            return Response({'status': 'error', 'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = serializers.UserSerializer(user)
-        return Response(serializer.data)
-
-class LogOutView(APIView):
-    def post(self, request):
-        response = Response()
-        response.delete_cookie('token')
-        response.data = {'message': 'Logout successful'}
-        return response
+    
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    try:
+        refresh_token = request.data.get('refresh_token')
+        if refresh_token:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({'status': 'success', 'message': 'Logged out successfully'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'status': 'error', 'message': 'Refresh token is required'}, status=status.HTTP_400_BAD_REQUEST)
+    except TokenError as e:
+        return Response({'status': 'error', 'message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
